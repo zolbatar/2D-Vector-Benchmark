70637a3d1cb2b4e99306af2369aa5418dcf2d65d
Daryl Dudey <daryljdudey@googlemail.com>
2016-05-30 23:09:21 +0100

    Python

--- Python/2dvector.py
+++ Python/2dvector.py
@@ -9,29 +9,17 @@ name = 'ball_glut'
 def main():
     glutInit(sys.argv)
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
-    glutInitWindowSize(400, 400)
+    glutInitWindowSize(1024, 768)
     glutCreateWindow(name)
 
-    glClearColor(0., 0., 0., 1.)
+    glClearColor(0.0, 0.0, 0.0, 1.0)
     glShadeModel(GL_SMOOTH)
-    glEnable(GL_CULL_FACE)
     glEnable(GL_DEPTH_TEST)
-    glEnable(GL_LIGHTING)
-    lightZeroPosition = [10., 4., 10., 1.]
-    lightZeroColor = [0.8, 1.0, 0.8, 1.0]  # green tinged
-    glLightfv(GL_LIGHT0, GL_POSITION, lightZeroPosition)
-    glLightfv(GL_LIGHT0, GL_DIFFUSE, lightZeroColor)
-    glLightf(GL_LIGHT0, GL_CONSTANT_ATTENUATION, 0.1)
-    glLightf(GL_LIGHT0, GL_LINEAR_ATTENUATION, 0.05)
-    glEnable(GL_LIGHT0)
     glutDisplayFunc(display)
     glMatrixMode(GL_PROJECTION)
-    gluPerspective(40., 1., 1., 40.)
+    glLoadIdentity()
+    glOrtho(0, 1024, 0, 768, -1, 1)
     glMatrixMode(GL_MODELVIEW)
-    gluLookAt(0, 0, 10,
-              0, 0, 0,
-              0, 1, 0)
-    glPushMatrix()
     glutMainLoop()
     return
 
@@ -39,9 +27,14 @@ def main():
 def display():
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glPushMatrix()
-    color = [1.0, 0., 0., 1.]
-    glMaterialfv(GL_FRONT, GL_DIFFUSE, color)
-    glutSolidSphere(2, 20, 20)
+
+    glLineWidth(1.0)
+    glColor3f(1.0, 0.0, 0.0)
+    glBegin(GL_LINES)
+    glVertex3f(0.0, 0.0, 0.0)
+    glVertex3f(100.0, 100.0, 0)
+    glEnd()
+
     glPopMatrix()
     glutSwapBuffers()
     return
